7b55338efcc20f7bce0b510fc0bd5136a9cd658a
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-07-24 16:33:02 +0530

    [init]: some project config changes and some architecture diagram

---
+++ include/image_processing.hpp
@@ -0,0 +1,10 @@
+#include <Magick++.h>
+
+namespace cppwal {
+class ImageProcessing {
+  std::string path;
+  std::vector<Magick::Color> colors;
+};
+} // namespace cppwal
+
+
--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,15 @@
-#include "Magick++/Color.h"
-#include "Magick++/Include.h"
 #include <Magick++.h>
 #include <iomanip>
+#include <iostream>
 #include <json/json.h>
 #include <map>
 #include <print>
-#include <iostream>
 #include <sstream>
+#include "Magick++/Color.h"
+#include "Magick++/Include.h"
 
 namespace Magick {
-std::string colorToHex(const Magick::Color &color) {
+std::string colorToHex(const Magick::Color& color) {
   auto ss = std::stringstream();
   unsigned int r = color.quantumRed() * 255 / QuantumRange;
   unsigned int g = color.quantumGreen() * 255 / QuantumRange;
@@ -22,7 +22,7 @@ std::string colorToHex(const Magick::Color &color) {
 }
 }; // namespace Magick
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   Magick::InitializeMagick(*argv);
   auto image = Magick::Image();
   for (int i = 1; i < argc; ++i) {
@@ -34,19 +34,28 @@ int main(int argc, char **argv) {
       auto hist_map = std::map<Magick::Color, size_t>();
       Magick::colorHistogram(&hist_map, image);
       auto colors_hex = std::vector<std::pair<Magick::Color, size_t>>();
-      for (const auto &[key, value] : hist_map) {
+      for (const auto& [key, value] : hist_map) {
         colors_hex.push_back(std::make_pair(key, value));
       }
-      std::sort(
-          colors_hex.begin(), colors_hex.end(),
-          [](auto pair1, auto pair2) { return pair1.second > pair2.second; });
+      std::sort(colors_hex.begin(), colors_hex.end(),
+                [](auto pair1, auto pair2) { return pair1.second > pair2.second; });
       auto j = Json::Value();
-      for (const auto &[idx, value] : std::views::enumerate(colors_hex)) {
-	j[std::format("color{}", idx)] = Magick::colorToHex(value.first);
+      for (const auto& [idx, value] : std::views::enumerate(colors_hex)) {
+        j[std::format("color{}", idx)] = Magick::colorToHex(value.first);
       }
       std::cout << j << std::endl;
-    } catch (Magick::Exception &e) {
+    } catch (Magick::Exception& e) {
       std::print("{}", e.what());
     }
   }
 }
+
+/*
+ - what main.cpp is suppose to do
+ - call the image processing cstor and initialize
+ - process the images but in a parallel way
+ - so the image_processing class is supposed to do that in parallel
+ - then collect all the colors and sort it and keep it
+ - then write a function to get the copy of vector to pass it to other classes that implement formatted_write_to_file
+-
+ */
