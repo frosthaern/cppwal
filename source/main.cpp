75c0a3962678ea47643714b8c8179bcfe9b2aa70
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-07-25 22:12:03 +0530

    [feat]: testing and printing if pixels are found

--- source/main.cpp
+++ source/main.cpp
@@ -1,39 +1,24 @@
 #include <Magick++.h>
+#include <algorithm>
+#include <filesystem>
 #include <iostream>
-#include <json/json.h> 
+#include <json/json.h>
 #include <print>
 #include <ranges>
-#include <filesystem>
 #include "image_processing.hpp"
-
 
 int main(int argc, char** argv) {
   Magick::InitializeMagick(*argv);
-  auto image = Magick::Image();
-  auto path = std::filesystem::absolute(argv[1]);
-  for (int i = 1; i < argc; ++i) {
-    try {
-      image.read(path.string());
-      image.quantizeColorSpace(Magick::RGBColorspace); // there is probably a cropping part missing somewhere
-      image.quantizeColors(16);
-      image.quantize();
-      auto hist_map = std::map<Magick::Color, size_t>();
-      Magick::colorHistogram(&hist_map, image);
-      auto colors_hex = std::vector<std::pair<Magick::Color, size_t>>();
-      for (const auto& [key, value] : hist_map) {
-        colors_hex.push_back(std::make_pair(key, value));
-      }
-      std::sort(colors_hex.begin(), colors_hex.end(),
-                [](auto pair1, auto pair2) { return pair1.second > pair2.second; });
-      auto j = Json::Value();
-      for (const auto& [idx, value] : std::ranges::views::enumerate(colors_hex)) {
-        j[std::format("color{}", idx)] = cppwal::colorToHex(value.first);
-      }
-      std::cout << j << std::endl;
-    } catch (Magick::Exception& e) {
-      std::print("{}", e.what());
-    }
-  }
+  auto paths = std::vector<std::filesystem::path>();
+  for (int i = 1; i < argc; ++i)
+    paths.push_back(std::filesystem::absolute(argv[i]));
+  auto image_processor = cppwal::ImageProcessing(paths);
+  image_processor.extract_colors();
+  auto colors = image_processor.get_colors();
+
+  std::for_each_n(colors.begin(), 10, [](auto color) {
+    std::println("Red: {}\nGreen: {}\nBlue: {}\nPixels: {}\n", color.first.quantumRed(), color.first.quantumGreen(), color.first.quantumBlue(), color.second);
+  });
 }
 
 /*
@@ -42,7 +27,7 @@ int main(int argc, char** argv) {
  - process the images but in a parallel way
  - so the image_processing class is supposed to do that in parallel
  - then collect all the colors and sort it and keep it
- - then write a function to get the copy of vector to pass it to other classes that implement formatted_write_to_file
+ - then write a function to get the copy of vector to pass it to other classes
+that implement formatted_write_to_file
 -
  */
-  
